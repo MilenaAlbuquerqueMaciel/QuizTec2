1945f1e2bc5cf04378748d2a2aab6d7e8f9cdb02
Milena Maciel <milena.maciel@duque.g12.br>
2024-09-26 21:35:32 -0300

    criamos a funcionalidade de responder as questoes do quiz

---
+++ src/main/java/br/com/antonioalves/quiz/Util.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.antonioalves.quiz;
+
+/**
+ *
+ * @author 08195
+ */
+public class Util {
+    public static void main(String[] args) {
+        System.out.println("Diretorio atual"+System.getProperty("user.dir"));
+    }
+    
+}
